ddca333b679c6fcff0f35a949a755fb24a726d28
c-fonseca <deboraaragao2004@gmail.com>
2025-11-26 21:30:52 -0300

    atualização de rotina

--- cutediary/public/script.js
+++ cutediary/public/script.js
@@ -5,77 +5,60 @@ const daysContainer = document.getElementById("days")
 const prevMonth = document.getElementById("prev-month")
 const nextMonth = document.getElementById("next-month")
 
-// INSTANCIAÇÃO DO OBJETO QUE IRÁ ARMAZENAR A DATA QUE 
-// O USUÁRIO ESTÁ VIZUALIZANDO ATUALMENTE NO CALENDÁRIO
-let currentDate = new Date()
+// PARA RECEBER A DATA ATUAL
+let currentDay = new Date()
 
-// FUNÇÃO QUE VAI GERAR O CALENDÁRIO 
 function renderCalendar(date){
+	const year = date.getFullYear() // RETORNA O ANO 
+	const month = date.getMonth() // RETORNA O MÊS
+	
+	// VIZUALIZAÇÃO NO MÊS E ANO DO CALENDÁRIO
+	const monthName = date.toLocaleString("pt-BR", {month: "long"})
+	let monthCap = monthName.charAt(0).toUpperCase() + monthName.slice(1)
 
-    const year = date.getFullYear() // ARMAZENA O ANO DA DATA FORNECIDA
-    const month = date.getMonth() // ARMAZENA O MÊS DA DATA FORNECIDA
-    const monthName = date.toLocaleString("pt-BR", { month: "long"}); // ARMAZENA O NOME DO MÊS 
-    let monthCap = monthName.charAt(0).toUpperCase() + monthName.slice(1) // FORMATA O NOME DO MÊS PARA EXIBIÇÃO
-
-    // ATUALIAÇÃO DO MÊS E ANO NO TOPO DO CALENDÁRIO 
     monthYear.textContent = `${monthCap} de ${year}`
 
-    // INSTANCIAÇÃO DO OBJETO QUE IRÁ ARMAZENAR A DATA ATUAL REAL
-    // PARA COMPARAÇÃO NA LÓGICA DESTA FUNÇÃO
-    const today = new Date()
+	// INSTANCIAÇÃO DO OBEJTO QUE REBE A DATA QUE SERÁ COMPARADA NA LÓGICA DESTA FUNÇÃO
+	const today = new Date()
 
-    // RETORNA O DIA DA SEMANA EM QUE O 1° DIA DO MÊS COMEÇA
-    const firstDayMonth = new Date(year, month, 1).getDay()
-
-    // RETORNA O ÚLTIMO DIA DO PRÓXIMO MÊS
-    const lastDateMonth = new Date(year, month + 1, 0).getDate();
+	// RETORNA O DIA DA SEMANA CORRESPONTE AO 1° DIA DO MÊS
+	const firstDayMonth = new Date(year, month, 1).getDay()
+	
+	// RETORNA O ÚLTIMO DIA DO MÊS ATUAL
+	const lastDayMonth = new Date(year, month + 1, 0).getDate()
 
     daysContainer.innerHTML = ""
 
-    // GERA OS ESPAÇOS VAZIOS
-    for(let i=0; i<=firstDayMonth; i++){
-        daysContainer.innerHTML += "<div></div>"
-    }
+    // LOOP PARA GERAR ESPAÇOS VAZIOS NO CALENDÁRIO
+	for(let i=0; i<firstDayMonth; i++){
+		daysContainer.innerHTML = "<div></div>"
+	}
+	
+    // LOOP PARA GERAR DIAS NUMERADOS NO CALENDÁRIO
+	for(let i=1; i<=lastDayMonth; i++){
+		const dayElement = document.createElement("div")
+        dayElement.textContent = i
 
-    // GERA OS DIAS NUMERADOS DO MÊS
-    for(let i=1; i<=lastDateMonth; i++){
-        const dayElement = document.createElement("div")
-        dayElement.textContent = i;
-
-        if (year === today.getFullYear() && month === today.getMonth() && i === today.getDate()) {
-            dayElement.classList.add("active"); 
+        // PARA MARCAR O DIA ATUAL
+        if(year === today.getFullYear() && month === today.getMonth() && i === today.getDate()){
+            dayElement.classList.add("active")
         }
 
-        daysContainer.appendChild(dayElement);
-    }
-
+        // PARA INSERIR OS ELEMENTOS
+        daysContainer.appendChild(dayElement)
+	}
 }
 
- prevMonth.addEventListener("click", () => {
-        currentDate.setMonth(currentDate.getMonth() - 1)
-        renderCalendar(currentDate)
-    })
+// PARA NAVEGAÇÃO NO CALENDÁRIO
+prevMonth.addEventListener("click", () => {
+    currentDay.setMonth(currentDay.getMonth() - 1)
+    renderCalendar(currentDay)
+})
 
-    nextMonth.addEventListener("click", () => {
-        currentDate.setMonth(currentDate.getMonth() + 1)
-        renderCalendar(currentDate)
-    })
+nextMonth.addEventListener("click", () => {
+    currentDay.setMonth(currentDay.getMonth() + 1)
+    renderCalendar(currentDay)
+})
 
-
-renderCalendar(currentDate)
-
-// PARA FIXAÇÃO DE LÓGICA
-
-// currentDay = new Date()
-
-// function gerarCalendario(data){
-
-//     const ano = data.getFullYear()
-//     const mes = data.getMonth()
-
-//     const hoje = new Date()
-
-//     const primeiroDiaDoMes = new Date(ano, mes, 1).getDay
-
-//     const ultimoDiaDoMes = new Date(ano, mes + 1, 0)
-// }
+// PARA RODAR FUNÇÃO
+renderCalendar(currentDay)
